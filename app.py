388876e9ba40c762de04307b42979d42fb23c3cb
iMAC <imac@iMac-de-iMAC.local>
2020-10-04 22:16:38 +0100

    added register screen

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-from flask import Flask,render_template
-from flask_assets import Environment,Bundle
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
 from config import Config
 from forms.login import LoginForm
+from forms.register import RegisterForm
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
 
-
-
-
-
-
-@app.route('/')
-def hello_world():
-
-
+@app.route('/login')
+def login():
     loginForm = LoginForm()
-    return render_template('auth/login.html',form=loginForm)
+    return render_template('auth/login.html', form=loginForm)
+
+
+@app.route('/register')
+def register():
+    registerForm = RegisterForm()
+    return render_template('auth/Register.html', form=registerForm)
 
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(port=8000,debug=True)
+    app.run(port=8000, debug=True)
--- forms/register.py
+++ forms/register.py
@@ -2,7 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired,Length
 
-class LoginForm(FlaskForm):
+class RegisterForm(FlaskForm):
+    full_names = StringField('Full Names', validators=[DataRequired(), Length(min=6, max=200)])
     email = StringField('Email',validators=[DataRequired(), Length(min=6, max=35)])
     password = PasswordField('Password',validators=[DataRequired()])
-    submit = SubmitField('Log In')
+    submit = SubmitField('Register')
